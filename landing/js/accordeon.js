bfb0a270d0e563bc139b3a0f30ff29522f7c8018
Rus <ruslan201010@gmail.com>
2017-10-08 23:41:40 +0300

    S-quote add

---
+++ landing/js/accordeon.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+	$('ul.accordeon li > p').click(function() {
+		if(!$(this).hasClass('active')) {
+			$('ul.accordeon li > p').removeClass('active').next('div').slideUp();
+			$(this).addClass('active');
+			$(this).next('div').slideDown(200);
+		} else {
+			$(this).removeClass('active').next('div').slideUp();
+			}
+		});
+
+		$("ul.accordeon li.accord1").bind("click", function() {
+      var src = ($("#accord_arrow1").attr("src") === "images/sservice-ARROW _ DOWN.png")
+                    ? "images/ARROW_UP.png" 
+                    : "images/sservice-ARROW _ DOWN.png";
+      $("#accord_arrow1").attr("src", src);
+		});
+		
+		$("ul.accordeon li.accord2").bind("click", function() {
+      var src = ($("#accord_arrow2").attr("src") === "images/sservice-ARROW _ DOWN.png")
+                    ? "images/ARROW_UP.png" 
+                    : "images/sservice-ARROW _ DOWN.png";
+      $("#accord_arrow2").attr("src", src);
+		});
+		$("ul.accordeon li.accord3").bind("click", function() {
+      var src = ($("#accord_arrow3").attr("src") === "images/sservice-ARROW _ DOWN.png")
+                    ? "images/ARROW_UP.png" 
+                    : "images/sservice-ARROW _ DOWN.png";
+      $("#accord_arrow3").attr("src", src);
+		});
+	});

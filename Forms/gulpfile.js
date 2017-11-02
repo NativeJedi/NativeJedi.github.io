b69a396afdfe7bf88f1b3de3e11a7e597740537e
Rus <ruslan201010@gmail.com>
2017-11-02 02:01:17 +0200

    Non-fix

--- Forms/gulpfile.js
+++ Forms/gulpfile.js
@@ -1,97 +1,107 @@
 var $            = require('gulp-load-plugins')();
 var gulp 				 = require('gulp'),
-		browser      = require('browser-sync').create(),
-		autoprefixer = require('autoprefixer'),
-		postcss 		 = require('gulp-postcss'),
-    del          = require('del'),
-    combiner     = require('stream-combiner2').obj,
-    pngquant		= require('imagemin-pngquant'),
-    iconfont     = require('gulp-iconfont'),
-    newer				 = require('gulp-newer'),
-    consolidate  = require('gulp-consolidate'),
-    svgstore     = require('gulp-svgstore'),
-    svgmin = require('gulp-svgmin');
+browser      = require('browser-sync').create(),
+autoprefixer = require('autoprefixer'),
+postcss 		 = require('gulp-postcss'),
+del          = require('del'),
+combiner     = require('stream-combiner2').obj,
+pngquant		 = require('imagemin-pngquant'),
+iconfont     = require('gulp-iconfont'),
+newer				 = require('gulp-newer'),
+consolidate  = require('gulp-consolidate'),
+svgstore     = require('gulp-svgstore'),
+svgmin       = require('gulp-svgmin'),
+rename       = require('gulp-rename'),
+inject       = require('gulp-inject'),
+pug          = require('pug');
+
+var svgWatch            = './web/webroot/WEB-INF/_svg-src/*.svg';
+var svgIconsSource      = './web/webroot/WEB-INF/tags/responsive/template/svg.html';
+var svgIconsDestination = './web/webroot/WEB-INF/tags/responsive/template';
 
 gulp.task('svgstore', function () {
-    return gulp
-        .src('assets/icons/*.svg')
-        .pipe(svgmin(function (file) {
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        prefix: 'svg-',
-                        minify: true
-                    }
-                }]
-            }
-        }))
-        .pipe(svgstore())
-        .pipe(gulp.dest('dist/svg'));
+
+ var svgs = gulp
+
+ .src('assets/icons/*.svg')
+ .pipe(rename({prefix: 'svg-'}))
+ .pipe(svgmin())
+ .pipe(svgstore({ inlineSvg: true }));
+
+ function fileContents (filePath, file) {
+   return file.contents.toString();
+ }
+
+ return gulp
+ .src('./index.html')
+ .pipe(inject(svgs, { transform: fileContents }))
+ .pipe(gulp.dest('./'));
+
 });
 
 gulp.task("build:icons", function() {
     return gulp.src(["./assets/icons/*.svg"]) //path to svg icons
-      .pipe(iconfont({
-        fontName: "myicons",
-        formats: ["ttf", "eot", "woff", "svg"],
-        centerHorizontally: true,
-        fixedWidth: true,
-        normalize: true,
-        fontHeight: 500
+    .pipe(iconfont({
+      fontName: "myicons",
+      formats: ["ttf", "eot", "woff", "svg"],
+      centerHorizontally: true,
+      fixedWidth: true,
+      normalize: true,
+      fontHeight: 500
       }))
-      .on("glyphs", (glyphs) => {
+    .on("glyphs", (glyphs) => {
 
         gulp.src("./assets/icons/util/*.scss") // Template for scss files
-            .pipe(consolidate("lodash", {
-                glyphs: glyphs,
-                fontName: "myicons",
-                fontPath: "../fonts/"
-            }))
+        .pipe(consolidate("lodash", {
+          glyphs: glyphs,
+          fontName: "myicons",
+          fontPath: "../fonts/"
+          }))
             .pipe(gulp.dest("./assets/scss/icons/")); // generated scss files with classes
-      })
+            })
       .pipe(gulp.dest("./dist/assets/fonts/")); //icon font destination
-});
+      });
 
 gulp.task('img', function() {
   return combiner(
   	gulp.src('./assets/img/**/*'),
     $.imagemin({
-			interlaced: true,
-			progressice: true,
-			svgoPlugins: [{removeViewBox: false}],
-			une: [pngquant()]
-		 }),
+     interlaced: true,
+     progressice: true,
+     svgoPlugins: [{removeViewBox: false}],
+     une: [pngquant()]
+     }),
     gulp.dest('./dist/assets/img')
-  ).on('error', $.notify.onError());
-});
+    ).on('error', $.notify.onError());
+  });
 
 gulp.task('sass', function () {
  return combiner(
-    gulp.src('assets/scss/**/*.scss'),
-    $.sourcemaps.init(),
-    $.sass({
-        output_style: 'compressed',
-        includePaths: ['node_modules/foundation-sites/scss']
-      }),
-    postcss([ autoprefixer({ browsers: ['last 3 version'] }) ]),
-    $.sourcemaps.write('.'),
-    gulp.dest('./dist/assets/css'),
-    browser.stream({match: '**/*.css'})
+  gulp.src('assets/scss/**/*.scss'),
+  $.sourcemaps.init(),
+  $.sass({
+    output_style: 'compressed',
+    includePaths: ['node_modules/foundation-sites/scss']
+    }),
+  postcss([ autoprefixer({ browsers: ['last 3 version'] }) ]),
+  $.sourcemaps.write('.'),
+  gulp.dest('./dist/assets/css'),
+  browser.stream({match: '**/*.css'})
   ).on('error', $.notify.onError());
-});
+ });
 
 // Starts a BrowerSync instance
 gulp.task('serve', ['sass'], function(){
   browser.init({
-        server: {
-            baseDir: "./"
-        }
+    server: {
+      baseDir: "./"
+    }
     });
-});
+  });
 
 gulp.task('clean', function() {
   return del('./dist/**/*');
-});
+  });
 
 gulp.task('build', ['clean', 'img', 'build:icons', 'sass']);
 
@@ -99,4 +109,4 @@ gulp.task('build', ['clean', 'img', 'build:icons', 'sass']);
 gulp.task('default', ['serve'], function() {
   gulp.watch(['assets/scss/**/*.scss'], ['sass']);
   gulp.watch('./**/*.html').on('change', browser.reload);
-});
+  });

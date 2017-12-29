902bfca60e5cc7573db4a22d3fca2f0301cd099d
ruslan <ruslan201010@gmail.com>
2017-12-29 20:08:29 +0200

    slider func

--- Projects/slider/assets/libs/common.js
+++ Projects/slider/assets/libs/common.js
@@ -1,37 +1,63 @@
-var images = document.querySelectorAll('.slider__gallery img');
-var next = document.querySelector('.btn--next');
-var prev = document.querySelector('.btn--prev');
-var slider = new Slider(images);
 
-next.onclick = function() {
-	slider.next();
-};
-
-prev.onclick = function() {
-	slider.prev();
+var sliderSettings = {
+	slider: document.querySelector('.slider'),
+	images: document.querySelectorAll('.slider img'),
+	btnNext: document.querySelector('.btn--next'),
+	btnPrev: document.querySelector('.btn--prev'),
+	autoplay: 3000
 }
 
+var slider = new Slider(sliderSettings);
 
-function Slider(images) {
-	var i = 0;
+function Slider(set) {
+	let i = 0;
+	let that = {};
 
-	this.images = images;
+	this.btnNext = set.btnNext;
+	this.btnPrev = set.btnPrev;
+	this.body  	 = set.slider;
 
-	this.next = function() {
-		this.images[i].classList.remove("active");
+	that.images  = set.images;
+
+	that.next = function() {
+		that.images[i].classList.remove("active");
 		i++;
-		if (i >= this.images.length) {
+		if (i >= that.images.length) {
 			i = 0;
 		}
-		this.images[i].classList.add("active");
-	}
+		that.images[i].classList.add("active");
+	};
 
-	this.prev = function() {
-		this.images[i].classList.remove("active");
+	that.prev = function() {		
+		that.images[i].classList.remove("active");
 		i--;
 		if (i === -1) {
-			i = this.images.length - 1;
+			i = that.images.length - 1;
 		}
-		this.images[i].classList.add("active");
+		that.images[i].classList.add("active");
+	};
+
+	this.btnNext.addEventListener('click', that.next);
+
+	this.btnPrev.addEventListener('click', that.prev);
+
+	if (set.autoplay) {
+		var timeId;
+
+		timeId = setInterval(that.next, set.autoplay);
+
+		this.body.addEventListener('mouseover', function() {
+			clearInterval(timeId);
+		});
+
+		this.body.addEventListener('mouseout', function() {
+			timeId = setInterval(that.next, set.autoplay);
+		});
 	}
+
+	for( let j = 0; j < that.images.length; j++) {
+		that.images[j].addEventListener('click', that.next);
+	}
+
 }
+

11b5872a092e2a7387df252ce127f9f505ffab3e
ruslan <ruslan201010@gmail.com>
2018-01-05 20:36:01 +0200

    sort adding

--- Projects/test/assets/libs/common.js
+++ Projects/test/assets/libs/common.js
@@ -1,20 +1,20 @@
 window.onload = function() {
 
 	var btn = document.querySelector('.btn-get');
-	var inp = document.querySelector('.search');
+	var input = document.querySelector('.search');
+	var table = document.querySelector('.table-data');
 
-	inp.addEventListener('input', function() {
+	//Data seaching on input event
+
+	input.addEventListener('input', function() {
 		search();
 	});
 
 	//loading data on button click 
 
-	btn.addEventListener('click', function() {
+	btn.addEventListener('click', function(e) {
 
 		ajaxGet('test.json', function(data) {
-
-			// Sould to convert json to js-object at first
-
 			
 			var table = document.querySelector('.table-data tbody');
 
@@ -22,6 +22,8 @@ window.onload = function() {
 				alert('Data is already loaded!');
 				return false;
 			}
+
+			// Sould to convert json to js-object at first
 
 			var json = JSON.parse(data);
 
@@ -51,14 +53,23 @@ window.onload = function() {
 
 			// Currency value after rows creating
 
-			table.innerHTML += '<tr><td colspan="5" >Currency: '
-			+ currency + '</td><tr>';
+			table.innerHTML += '<tr><td id="total" colspan="5" >Currency: '
+			+ currency + '</td></tr>';
 		});
 
 	});
 
-}
 
+	//Sorting data
+
+	table.addEventListener('click', function(e){
+		if (e.target.tagName != 'TH') return;
+
+		sortTable(e.target.cellIndex, e.target.getAttribute('data-type'), this);
+
+	});
+
+}
 
 function ajaxGet(url, callback) {
 
@@ -83,7 +94,6 @@ function ajaxGet(url, callback) {
 function search() {
 
 	var input = document.querySelector('.search').value;
-	var table = document.querySelector('.table-data');
 	var td = document.querySelectorAll('.table-data td');
 	var tr = document.querySelectorAll('.table-data tr');
 
@@ -95,25 +105,98 @@ function search() {
 
 	for(var i = 0, length1 = td.length; i < length1; i++){
 		var str = td[i].textContent.toLowerCase();
-	
+
 		if (str.indexOf(inp) + 1 && inp) {
 			td[i].classList.add('active');
 		} else if (str.indexOf(inp) == -1 || !inp) {
 			td[i].classList.remove('active');
 		}
+
 	}
 
 	// Loop for hiding rows which don't have searching data
 
 	for(var i = 1; i < tr.length; i++) {
+
 		for(var j = 0; j < tr[i].children.length; j++) {
 			if (tr[i].children[j].classList.contains('active') || !inp) {
-				tr[i].style.display = 'table-row';
+				tr[i].classList.remove('hide');
 				break;
 			} else {
-				tr[i].style.display = 'none';
+				tr[i].classList.add('hide');
 			}
+
 		}
 	}
+
+}
+
+function sortTable(colNum, type, table) {
+
+	var tbody = table.querySelector('tbody');
+
+	//The variable which contains currency row HTML
+
+	var total = tbody.querySelector('#total').outerHTML;
+
+	//We need to remove row with currency value for sorting
+	//only data
+
+	tbody.removeChild(tbody.lastChild);
+
+	//Array which contains rows of data
+
+	var rowsArray = [].slice.call(tbody.rows);
+	
+
+	//This I took from learn.js in the best traditions 
+	// of the communnism:D
+	//But I little bit changed algoritm of string-sorting
+
+	//The function is determined by data-type
+
+	var compare;
+
+	switch (type) {
+		case 'number':
+		compare = function(rowA, rowB) {
+
+			//function is compating only data with the same 
+			//index as table header
+
+			return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+		};
+		break;
+
+		case 'string':
+
+		compare = function(rowA, rowB) {
+			if (rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML) {
+				return 1;
+			} else if (rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML) {
+				return -1;
+			} else { return 0; }
+		};
+
+		break;
+	}
+
+	rowsArray.sort(compare);
+
+	//Removing body for data rendering
+
+	table.removeChild(tbody);
+
+	//creating body again with sort data
+
+	for (var i = 0; i < rowsArray.length; i++) {
+		tbody.appendChild(rowsArray[i]);
+	}
+
+	table.appendChild(tbody);
+
+	// And creating total value
+
+	tbody.innerHTML += total;
 
 }

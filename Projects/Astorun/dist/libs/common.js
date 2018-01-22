5b35defabaf9a759730558262b81618438897a78
ruslan <ruslan201010@gmail.com>
2018-01-22 23:48:44 +0200

    news added

--- Projects/Astorun/dist/libs/common.js
+++ Projects/Astorun/dist/libs/common.js
@@ -1,6 +1,26 @@
-window.onload = function() {
+$( document ).ready(function() {
+	$('.b-news-slider').slick({
+		speed: 500,
+		cssEase: 'linear'
+	});
 
-};
+	var img = document.querySelectorAll('#slider-img');
+	var slider = document.querySelector('.b-news-slider');
+	var div = document.createElement('div');
+	div.className = 'slider-counter';
+	slider.appendChild(div);
+	var lengthImg = img.length;
+	div.innerHTML = '1 of ' + lengthImg;
+
+	$('.slick-arrow').on('click', function() {
+		newsCounter();
+	});
+
+	function newsCounter() {
+		var div = $('.slider-counter');
+		var img = $('.slick-active img');
+		div.html(img.data('count') + ' of ' + lengthImg);
+	}
+});
 
 
-	

8a3f15fa1b96a36e8a67b6b62d63426f05d2e9c1
ruslan201010@gmail.com <ruslan201010@gmail.com>
2018-03-09 01:17:00 +0200

    popup added

--- Projects/K2K/dist/libs/common.js
+++ Projects/K2K/dist/libs/common.js
@@ -11,6 +11,21 @@ window.onload = function() {
 	slider('#credit-slider', 0, 10000, ' грн');
 	slider('#termin-slider', 0, 50, 'дней');
 
+	var popup = new Popup();
+
+	$('.btnRegistration').on('click', function() {
+		popup.open($('.popup #form-reg'));
+	});
+
+	$('.btnLogin').on('click', function() {
+		popup.open($('.popup #form-login'));
+	});
+
+	$('.btnKeep').on('click', function(e) {
+		e.preventDefault();
+
+		popup.changeContent($('#form-confirm'));
+	});
 	function minHeight(selector) {
 		var currentHeight = selector.height();
 		console.log(currentHeight)
@@ -30,4 +45,34 @@ function slider(el, min, max, type) {
 			$(el + ' .ui-slider-val').html(ui.value + ' ' + type);
 		}
 	});
+}
+
+function Popup() {
+	var popup = $('.popup');
+	var self = this;
+	var popupFade = 200;
+	var contentFade = 200;
+  
+	self.open = function(content) {
+		self.content = content;
+		popup.fadeIn(popupFade);	
+		content.fadeIn(contentFade);
+	}
+
+  self.close = function(e) {
+  	var targ = e.target;
+
+  	if (!targ.classList.contains('popup') 
+  	&& !targ.classList.contains('popup__close')) return;
+  	$('.popup-content').fadeOut(contentFade);
+  	popup.fadeOut(popupFade);
+  }
+
+  self.changeContent = function(changeEl) {
+  	self.content.fadeOut(contentFade, function() {
+  		changeEl.fadeIn(contentFade);
+  	});
+  }
+
+  popup.on('click', self.close);
 }

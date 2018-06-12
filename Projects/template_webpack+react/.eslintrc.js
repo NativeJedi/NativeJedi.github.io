e7018e69affe8d6fa6330c2ace222cc9d53989bb
BlackJeezuz <ruslan201010@gmail.com>
2018-06-12 14:35:53 +0300

    vue push

---
+++ Projects/template_webpack+react/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'no-plusplus': 0,
+    'no-undef': 0,
+    'eol-last': 0,
+    'no-trailing-spaces': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};

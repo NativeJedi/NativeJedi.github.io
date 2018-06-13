a7688b9ab9135548b09081bd298e029c153626b1
BlackJeezuz <ruslan201010@gmail.com>
2018-06-13 12:34:54 +0300

    todo vue fixed

--- Projects/template_vue/src/components/TodoItem.vue
+++ Projects/template_vue/src/components/TodoItem.vue
@@ -3,7 +3,7 @@
     <label class="todo-item__label" v-bind:class="{'is-active': todo.active}" v-bind:for="'deal-' + todo.id" @click="onActive">
       <span class="todo-item__number">{{ this.index + 1 }}</span>{{ todo.text }}
     </label>
-    <button class="todo-item__remove" @click="onRemove">X</button>
+    <button class="todo-item__remove" @click="onRemove" />
   </li>
 </template>
 
@@ -40,16 +40,15 @@ export default {
     width: calc(100% - 50px);
     padding: 5px 40px;
     cursor: pointer;
-    font-weight: bold;
-
+    font-size: 20px;
     &:before {
       content: '';
       position: absolute;
       left: 10px;
       top: 50%;
       transform: translate(0, -50%);
-      width: 15px;
-      height: 15px;
+      width: 20px;
+      height: 20px;
       border-radius: 5px;
       border: 1px solid #ccc;
       text-align: center;
@@ -70,22 +69,50 @@ export default {
   }
 
   &__remove {
-    max-width: 20px;
+    position: relative;
+    width: 25px;
+    height: 25px;
     padding: 3px 5px;
     font-size: 10px;
     background-color: transparent;
+    border: 2px solid transparent;
+    border-radius: 50%;
     cursor: pointer;
+    transition: all 0.4s;
+    &:after,
+    &:before {
+      content: '';
+      width: 3px;
+      height: 80%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      background-color: #f06371;
+    }
+
+    &:after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+
+    &:before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &:hover {
+      border-color: #f06371;
+      transform: scale(1.1) rotate(90deg);
+    }
   }
 
   &__number {
     display: inline-block;
-    width: 15px;
-    height: 15px;
-    margin-right: 20px;
+    width: 20px;
+    height: 20px;
+    margin: 0 10px 0 20px;
     text-align: center;
-    line-height: 15px;
-    border: 1px solid #000;
-    font-size: 14px;
+    line-height: 20px;
+    border: 1px solid currentColor;
+    font-size: 0.8em;
     border-radius: 50%;
   }
 }
